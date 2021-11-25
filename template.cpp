cf3d0fa39cd4d9fb130b584754e03a2f538bcdb1
Rolf Schatten <Rolf.Schatten@gmail.com>
2021-11-25 19:04:10 +0100

    Added template

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int, char**) {
+    string input = "";
+    int result = 0;
+
+    for ( char c : input )
+    {
+    }
+
+    cout << result << endl;
+}
