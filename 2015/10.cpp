55bbac1a48aa12a4b4d4b160e51f9f0a33f81ed2
Rolf Schatten <Rolf.Schatten@gmail.com>
2021-11-28 02:37:07 +0100

    New: 2015.10

---
+++ 2015/10.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+string lockandsay( const string &input )
+{
+    stringstream retval;
+    uint16_t counter = 0;
+    char oldc = input[0];
+
+    for ( auto c : input )
+    {
+        if ( oldc == c )
+            ++counter;
+        else
+        {
+            retval << counter << oldc;
+            oldc = c;
+            counter = 1;
+        }
+    }
+
+    retval << counter << oldc;
+
+    return retval.str();
+}
+
+
+int main(int, char**) {
+
+    //string input("1");
+    string input("3113322113");
+
+    for ( int i = 1; i<=50; ++i )
+    {
+        input = lockandsay ( input );
+        cout << i << ". (" << input.length() << "): " << endl;
+    }
+
+    return 0;
+}
