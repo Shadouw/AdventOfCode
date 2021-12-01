b107f289a941e0f0a5aaffae7fce21c9f2809fbe
Rolf Schatten <Rolf.Schatten@gmail.com>
2021-12-01 17:24:49 +0100

    New: All 2015 template files

---
+++ 2015/20.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const string inputT[] = {
+""
+};
+
+const string inputA[] = {
+""
+};
+
+const string inputB[] = {
+""
+};
+
+
+auto &input = inputT;
+
+int main(int, char**) {
+
+    int resultA = 0;
+    int resultB = 0;
+
+    for ( auto elem : input )
+    {
+    }
+
+    cout << "Result A: " << resultA << endl;
+    cout << "Result B: " << resultB << endl;
+
+    return 0;
+}
