077c9247ea26da9fe25b44b960d8c7d992c19838
Rolf Schatten <Rolf.Schatten@gmail.com>
2021-12-08 09:33:12 +0100

    New: 2021.08

---
+++ 2021/08.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const string inputT[] = {
+""
+};
+
+const string inputA[] = {
+""
+};
+
+const string inputB[] = {
+""
+};
+
+
+auto &input = inputT;
+auto sizeofinput = sizeof(input) / sizeof(input[0]);
+
+int main(int, char**) {
+
+    int resultA = 0;
+    int resultB = 0;
+
+    for ( auto elem : input )
+    {
+    }
+
+    cout << "Result A: " << resultA << endl;
+    cout << "Result B: " << resultB << endl;
+
+    return 0;
+}
