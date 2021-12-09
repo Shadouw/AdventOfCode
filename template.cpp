40ba169ce944e9a7f58d1986a12e989564a4d4d7
Rolf Schatten <Rolf.Schatten@gmail.com>
2021-12-09 12:05:06 +0100

    Solved: 2021.09

--- template.cpp
+++ template.cpp
@@ -4,27 +4,24 @@
 using namespace std;
 
 const string inputT[] = {
-""
-};
+    ""};
 
 const string inputA[] = {
-""
-};
+    ""};
 
 const string inputB[] = {
-""
-};
-
+    ""};
 
 auto &input = inputT;
 auto sizeofinput = sizeof(input) / sizeof(input[0]);
 
-int main(int, char**) {
+int main(int, char **)
+{
     cout << "sizeofinput: " << sizeofinput << endl;
     int resultA = 0;
     int resultB = 0;
 
-    for ( auto elem : input )
+    for (auto elem : input)
     {
     }
 
