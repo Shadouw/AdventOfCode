cac9a1c049faeb2842ee10da54bd1b9471ceecb8
Rolf Schatten <Rolf.Schatten@gmail.com>
2021-12-14 16:54:06 +0100

    New: 2015.24

--- 2015/24.cpp
+++ 2015/24.cpp
@@ -1,34 +1,53 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
 using namespace std;
 
-const string inputT[] = {
-""
-};
+const vector<int> inputT = {1,2,3,4,5,7,8,9,10,11};
 
-const string inputA[] = {
-""
-};
+const vector<int> inputA = {1,2,3,5,7,13,17,19,23,29,31,37,41,43,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113};
 
-const string inputB[] = {
-""
-};
-
-
-auto &input = inputT;
-
-int main(int, char**) {
-
-    int resultA = 0;
-    int resultB = 0;
-
-    for ( auto elem : input )
+class problem
+{
+public:
+    problem(const vector<int> &_input) : input(_input)
     {
+        cout << "Size of Input: " << input.size() << endl;
+        for ( auto elem : input )
+            sumofproblem += elem;
+        cout << "Sum of Problem: " << sumofproblem << endl;
     }
 
-    cout << "Result A: " << resultA << endl;
-    cout << "Result B: " << resultB << endl;
+    void listinput()
+    {
+        for (auto elem : input)
+        {
+            cout << elem << endl;
+        }
+    }
 
-    return 0;
+    int getresultA(){
+        cout << "resultA: " << resultA << endl;
+        return resultA;
+    }
+    int getresultB(){
+        cout << "resultB: " << resultB << endl;
+        return resultB;
+    }
+private:
+    const vector<int>  input;
+    int sumofproblem = 0;
+    int resultA = 0;
+    int resultB = 0;
+};
+
+TEST_CASE ( "Testdata" )
+{
+    problem testproblem(inputT);
+    REQUIRE ( 0 == testproblem.getresultA() );
+    REQUIRE ( 0 == testproblem.getresultB() );
 }
