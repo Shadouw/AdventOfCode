0f94fd91ed8f0ee3cbcc5ff23d1d8bb8899b9379
Rolf Schatten <Rolf.Schatten@gmail.com>
2021-12-15 17:12:02 +0100

    Solved: 2021:15

--- 2021/15.cpp
+++ 2021/15.cpp
@@ -124,75 +124,136 @@ const vector<vector<uint32_t>> inputA = {
 class problem
 {
 public:
-    problem(const vector<vector<uint32_t>> &_input) : input(_input),
-                                                      xsize(_input[0].size()), ysize(_input.size())
+    problem(const vector<vector<uint32_t>> &_input, int scale) : originalinput(_input),
+                                                                 xsize(_input[0].size() * scale), ysize(_input.size() * scale),
+                                                                 input(ysize, std::vector<uint32_t>(xsize))
     {
         cout << "Size of Input: " << input.size() << endl;
-    }
 
-    int getresultA()
-    {
-        cout << "resultA: " << resultA << endl;
-        return resultA;
-    }
-    int getresultB()
-    {
-        cout << "resultB: " << resultB << endl;
-        return resultB;
+        uint32_t originalxsize = _input[0].size(), originalysize = _input.size();
+
+        for (int y = 0; y < ysize; ++y)
+        {
+            for (int x = 0; x < xsize; ++x)
+            {
+                input[y][x] = originalinput[y % originalysize][x % originalxsize] + (y / originalysize) + (x / originalxsize);
+
+                while (9 < input[y][x])
+                    input[y][x] -= 9;
+            }
+        }
+        /*
+        if (5 == scale)
+            for (int y = 0; y < ysize; ++y)
+            {
+                for (int x = 0; x < xsize; ++x)
+                {
+                    cout << input[y][x];
+                }
+                cout << endl;
+            }
+        */
     }
 
     uint32_t getRisk()
     {
         vector<vector<uint32_t>> RiskMatrix(ysize, std::vector<uint32_t>(xsize));
 
-        RiskMatrix[0][0] = 0;//input[0][0];
+        RiskMatrix[0][0] = 0; //input[0][0];
 
         // Set border of RiskMatrix
-        for (size_t i = 1; i < ysize; ++i)
-            RiskMatrix[i][0] = RiskMatrix[i - 1][0] + input[i][0];
+        for (size_t y = 1; y < ysize; ++y)
+            RiskMatrix[y][0] = RiskMatrix[y - 1][0] + input[y][0];
 
-        for (size_t j = 1; j < xsize; ++j)
-            RiskMatrix[0][j] = RiskMatrix[0][j - 1] + input[0][j];
+        for (size_t x = 1; x < xsize; ++x)
+            RiskMatrix[0][x] = RiskMatrix[0][x - 1] + input[0][x];
 
         // Now calculate the RiskMatrix
-        for (size_t i = 1; i < ysize; ++i)
+        for (size_t y = 1; y < ysize; ++y)
         {
-            for (size_t j = 1; j < xsize; ++j)
+            for (size_t x = 1; x < xsize; ++x)
             {
-                uint32_t distmin = RiskMatrix[i - 1][j];
-                if (RiskMatrix[i][j - 1] < distmin)
-                    distmin = RiskMatrix[i][j - 1];
-                /*
-                if (RiskMatrix[i - 1][j - 1] + c < distmin)
-                    distmin = RiskMatrix[i - 1][j - 1] + c;
-*/
-                RiskMatrix[i][j] = distmin + input[i][j];
+                uint32_t distmin = RiskMatrix[y - 1][x];
+                if (RiskMatrix[y][x - 1] < distmin)
+                    distmin = RiskMatrix[y][x - 1];
+
+                RiskMatrix[y][x] = distmin + input[y][x];
             }
         }
 
-        return RiskMatrix[ysize-1][xsize-1];
+        bool foundoptimization = true;
+        while (foundoptimization)
+        {
+            foundoptimization = false;
+
+            for (size_t y = 0; y < ysize; ++y)
+            {
+                for (size_t x = 0; x < xsize; ++x)
+                {
+                    uint32_t distmin = -1;
+
+                    if (0 < y)
+                        distmin = RiskMatrix[y - 1][x];
+
+                    if (0 < x)
+                        if (RiskMatrix[y][x - 1] < distmin)
+                            distmin = RiskMatrix[y][x - 1];
+
+                    if (ysize - 1 > y)
+                        if (RiskMatrix[y + 1][x] < distmin)
+                            distmin = RiskMatrix[y + 1][x];
+
+                    if (xsize - 1 > x)
+                        if (RiskMatrix[y][x + 1] < distmin)
+                            distmin = RiskMatrix[y][x + 1];
+
+                    if (RiskMatrix[y][x] > distmin + input[y][x])
+                    {
+                        foundoptimization = true;
+                        RiskMatrix[y][x] = distmin + input[y][x];
+                    }
+                }
+            }
+        }
+
+        return RiskMatrix[ysize - 1][xsize - 1];
     }
 
 private:
-    const vector<vector<uint32_t>> input;
+    const vector<vector<uint32_t>> originalinput;
     const uint32_t xsize;
     const uint32_t ysize;
     int resultA = 0;
     int resultB = 0;
+    vector<vector<uint32_t>> input;
 };
 
 TEST_CASE("Testdata")
 {
-    problem testproblem(inputT);
-    REQUIRE ( 40 == testproblem.getRisk() );
+    problem testproblem(inputT, 1);
+    REQUIRE(40 == testproblem.getRisk());
 }
 
-
-TEST_CASE("Problem")
+TEST_CASE("Testdata5")
 {
-    problem testproblem(inputA);
+    problem testproblem(inputT, 5);
+    REQUIRE(315 == testproblem.getRisk());
+}
+
+TEST_CASE("Problem A")
+{
+    problem testproblem(inputA, 1);
 
     uint32_t result = testproblem.getRisk();
     cout << "Result A: " << result << endl;
-    REQUIRE ( 811 == result );
+    REQUIRE(811 == result);
+}
+
+TEST_CASE("Problem B")
+{
+    problem testproblem(inputA, 5);
+
+    uint32_t result = testproblem.getRisk();
+    cout << "Result B: " << result << endl;
+    REQUIRE(3012 == result);
 }
