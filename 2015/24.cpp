482189ba53d9ae128762b934672f98cd492ff278
Rolf Schatten <Rolf.Schatten@gmail.com>
2021-12-19 00:28:57 +0100

    2015.24: Solved

--- 2015/24.cpp
+++ 2015/24.cpp
@@ -1,53 +1,203 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <set>
 
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
 using namespace std;
 
-const vector<int> inputT = {1,2,3,4,5,7,8,9,10,11};
+const set<uint32_t> inputT = {1, 2, 3, 4, 5, 7, 8, 9, 10, 11};
 
-const vector<int> inputA = {1,2,3,5,7,13,17,19,23,29,31,37,41,43,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113};
+const set<uint32_t> inputA = {1, 2, 3, 5, 7, 13, 17, 19, 23, 29,
+                              31, 37, 41, 43, 53, 59, 61, 67, 71,
+                              73, 79, 83, 89, 97, 101, 103, 107, 109, 113};
 
-class problem
+class packagegroup
 {
 public:
-    problem(const vector<int> &_input) : input(_input)
+    void addpackage(uint32_t package)
     {
-        cout << "Size of Input: " << input.size() << endl;
-        for ( auto elem : input )
-            sumofproblem += elem;
-        cout << "Sum of Problem: " << sumofproblem << endl;
+        packages.insert(package);
+        weight += package;
+        qe *= package;
     }
-
-    void listinput()
+    void removepackage(uint32_t package)
     {
-        for (auto elem : input)
+        auto const pos = packages.find(package);
+        if (pos != packages.end())
         {
-            cout << elem << endl;
+            packages.erase(pos);
+            weight -= package;
+            qe /= package;
         }
     }
 
-    int getresultA(){
-        cout << "resultA: " << resultA << endl;
-        return resultA;
-    }
-    int getresultB(){
-        cout << "resultB: " << resultB << endl;
-        return resultB;
-    }
-private:
-    const vector<int>  input;
-    int sumofproblem = 0;
-    int resultA = 0;
-    int resultB = 0;
+    set<uint32_t> packages;
+    uint32_t weight = 0;
+    uint64_t qe = 1;
 };
 
-TEST_CASE ( "Testdata" )
+class balance
 {
-    problem testproblem(inputT);
-    REQUIRE ( 0 == testproblem.getresultA() );
-    REQUIRE ( 0 == testproblem.getresultB() );
+public:
+    balance(const set<uint32_t> &_input, uint32_t _n) : input(_input), numofgroups(_n)
+    {
+        cout << "Size of Input: " << input.size() << endl;
+        for (auto elem : input)
+            sumofall += elem;
+        cout << "Sum of all packages: " << sumofall << " ";
+        assert(sumofall % numofgroups == 0);
+        sumofgroup = sumofall / numofgroups;
+        cout << "Sum of one package: " << sumofgroup << endl;
+
+        // array<packagegroup, numofgroups> resultsets;
+
+        // Much too slow.
+        //getallvalidgroups(resultsets, input.size() - 1);
+
+        packagegroup resultset;
+        getsmallestpackage(resultset, input.size() - 1);
+
+        cout << "Valid Groups: " << groups.size() << endl;
+    }
+
+    void getsmallestpackage(packagegroup &resultset, int level)
+    {
+        if (resultset.packages.size() > smallestpackagesize)
+            return;
+        if (resultset.packages.size() == smallestpackagesize && resultset.qe > smallestpackageqe)
+            return;
+        if (resultset.weight > sumofgroup)
+            return;
+
+        if (resultset.weight == sumofgroup)
+        {
+            groups.insert(resultset.packages);
+            return;
+        }
+
+        if (level < 0)
+            return;
+
+        auto element = (input.begin());
+        for (int i = 0; i < level; ++i)
+            ++element;
+        uint32_t currentNumber = *element;
+
+        // Two options: Add it or not
+        for (int i = 0; i < 2; ++i)
+        {
+            if (1 == i)
+                resultset.addpackage(currentNumber);
+            getsmallestpackage(resultset, level - 1);
+            if (1 == i)
+                resultset.removepackage(currentNumber);
+        }
+    }
+    /*
+    void getallvalidgroups(array<packagegroup, numofgroups> &resultsets, int level)
+    {
+        for (auto resultset : resultsets)
+            if (resultset.weight > sumofgroup)
+                return;
+
+        if (resultsets[0].weight == sumofgroup &&
+            resultsets[1].weight == sumofgroup &&
+            resultsets[2].weight == sumofgroup)
+        {
+            for (auto resultset : resultsets)
+                groups.insert(resultset.packages);
+            return;
+        }
+
+        if (level < 0)
+            return;
+
+        auto element = (input.begin());
+        for (int i = 0; i < level; ++i)
+            ++element;
+        uint32_t currentNumber = *element;
+
+        // How many targets?
+        int t = numofgroups;
+        if (0 == resultsets[0].weight)
+            t = 1;
+        else if (0 == resultsets[1].weight)
+            t = 2;
+
+        for (int i = 0; i < t; ++i)
+        {
+            if (resultsets[i].weight + currentNumber <= sumofgroup && level >= 0)
+            {
+                resultsets[i].addpackage(currentNumber);
+                getallvalidgroups(resultsets, level - 1);
+                resultsets[i].removepackage(currentNumber);
+            }
+        }
+    }
+*/
+    uint64_t getSmallestQE()
+    {
+        size_t smallestsize = -1;
+        for (auto group : groups)
+        {
+            if (group.size() < smallestsize)
+                smallestsize = group.size();
+        }
+
+        uint64_t smallestqe = -1;
+        for (auto group : groups)
+        {
+            if (group.size() == smallestsize)
+            {
+                uint64_t qe = 1;
+                for (auto packet : group)
+                    qe *= packet;
+
+                if (qe < smallestqe)
+                    smallestqe = qe;
+            }
+        }
+
+        return smallestqe;
+    }
+
+    uint32_t getNumOfGroups() { return groups.size(); }
+
+private:
+    const set<uint32_t> input;
+    uint32_t sumofall = 0;
+    uint32_t sumofgroup = 0;
+    uint32_t smallestpackagesize = -1;
+    uint64_t smallestpackageqe = -1;
+
+    const uint32_t numofgroups;
+
+    set<set<uint32_t>> groups;
+};
+
+TEST_CASE("Testdata")
+{
+    balance test(inputT, 3);
+    // Probably wrong: Example Data (19) is incomplete
+    // REQUIRE(24 == test.getNumOfGroups());
+    REQUIRE(99 == test.getSmallestQE());
+
+    balance testb(inputT, 4);
+    REQUIRE(44 == testb.getSmallestQE());
+}
+
+TEST_CASE("Task")
+{
+    balance problemA(inputA, 3);
+    uint64_t resultA = problemA.getSmallestQE();
+    cout << "Result A: " << resultA << endl;
+    REQUIRE(10723906903 == resultA);
+
+    balance problemB(inputA, 4);
+    uint64_t resultB = problemB.getSmallestQE();
+    cout << "Result B: " << resultB << endl;
+    REQUIRE(74850409 == resultB);
 }
