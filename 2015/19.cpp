d65017244263dc22e619f0066950a693b6832ed3
Rolf Schatten <Rolf.Schatten@gmail.com>
2021-12-22 08:46:58 +0100

    New: 2021.22

--- 2015/19.cpp
+++ 2015/19.cpp
@@ -10,8 +10,7 @@ const string inputT[][2] = {
     "H", "HO",
     "H", "OH",
     "O", "HH",
-    "HOHOHO", ""
-};
+    "HOHOHO", ""};
 
 const string inputA[][2] = {
     "Al", "ThF",
@@ -57,15 +56,14 @@ const string inputA[][2] = {
     "e", "HF",
     "e", "NAl",
     "e", "OMg",
-    "CRnCaSiRnBSiRnFArTiBPTiTiBFArPBCaSiThSiRnTiBPBPMgArCaSiRnTiMgArCaSiThCaSiRnFArRnSiRnFArTiTiBFArCaCaSiRnSiThCaCaSiRnMgArFYSiRnFYCaFArSiThCaSiThPBPTiMgArCaPRnSiAlArPBCaCaSiRnFYSiThCaRnFArArCaCaSiRnPBSiRnFArMgYCaCaCaCaSiThCaCaSiAlArCaCaSiRnPBSiAlArBCaCaCaCaSiThCaPBSiThPBPBCaSiRnFYFArSiThCaSiRnFArBCaCaSiRnFYFArSiThCaPBSiThCaSiRnPMgArRnFArPTiBCaPRnFArCaCaCaCaSiRnCaCaSiRnFYFArFArBCaSiThFArThSiThSiRnTiRnPMgArFArCaSiThCaPBCaSiRnBFArCaCaPRnCaCaPMgArSiRnFYFArCaSiThRnPBPMgAr", ""
-};
+    "CRnCaSiRnBSiRnFArTiBPTiTiBFArPBCaSiThSiRnTiBPBPMgArCaSiRnTiMgArCaSiThCaSiRnFArRnSiRnFArTiTiBFArCaCaSiRnSiThCaCaSiRnMgArFYSiRnFYCaFArSiThCaSiThPBPTiMgArCaPRnSiAlArPBCaCaSiRnFYSiThCaRnFArArCaCaSiRnPBSiRnFArMgYCaCaCaCaSiThCaCaSiAlArCaCaSiRnPBSiAlArBCaCaCaCaSiThCaPBSiThPBPBCaSiRnFYFArSiThCaSiRnFArBCaCaSiRnFYFArSiThCaPBSiThCaSiRnPMgArRnFArPTiBCaPRnFArCaCaCaCaSiRnCaCaSiRnFYFArFArBCaSiThFArThSiThSiRnTiRnPMgArFArCaSiThCaPBCaSiRnBFArCaCaPRnCaCaPMgArSiRnFYFArCaSiThRnPBPMgAr", ""};
 
 const string inputB[][2] = {
-    //"1", "5F",
-    //"1", "57F8",
+    "1", "5F",
+    "1", "57F8",
     "B", "B2",
     "B", "6B",
-    //"B", "67F8",
+    "B", "67F8",
     "2", "22",
     "2", "PB",
     "2", "P7F8",
@@ -75,25 +73,25 @@ const string inputB[][2] = {
     "F", "2F",
     "F", "P3",
     "F", "41",
-    //"H", "C718",
-    //"H", "C7FYFYF8",
-    //"H", "C7FY38",
-    //"H", "C73YF8",
-    //"H", "H2",
-    //"H", "N7FYF8",
-    //"H", "N738",
-    //"H", "N5",
+    "H", "C718",
+    "H", "C7FYFYF8",
+    "H", "C7FY38",
+    "H", "C73YF8",
+    "H", "H2",
+    "H", "N7FYF8",
+    "H", "N738",
+    "H", "N5",
     "H", "OB",
-    //"H", "O7F8",
+    "H", "O7F8",
     "3", "BF",
     "3", "63",
-    //"N", "C7F8",
-    //"N", "H4",
-    //"O", "C7FYF8",
-    //"O", "C738",
+    "N", "C7F8",
+    "N", "H4",
+    "O", "C7FYF8",
+    "O", "C738",
     "O", "HP",
-    //"O", "N7F8",
-    //"O", "O6",
+    "O", "N7F8",
+    "O", "O6",
     "P", "2P",
     "P", "P6",
     "P", "47F8",
@@ -102,44 +100,43 @@ const string inputB[][2] = {
     "6", "BP",
     "6", "66",
     "e", "HF",
-    //"e", "N1",
+    "e", "N1",
     "e", "O3",
-    "C7247B47F86BP66BF8PB245476BPBP38247638245247F8747F866BF8224745224738FY47FY2F845245PBP6382P7418PB2247FY4527F882247PB47F83Y222245224182247PB418B2222452PB45PBPB247FYF845247F8B2247FYF8452PB45247P387F8P6B2P7F82222472247FYF8F8B245F85454767P38F82452PB247BF822P722P3847FYF82457PBP38", ""
-
-};
+    "C7247B47F86BP66BF8PB245476BPBP38247638245247F8747F866BF8224745224738FY47FY2F845245PBP6382P7418PB2247FY4527F882247PB47F83Y222245224182247PB418B2222452PB45PBPB247FYF845247F8B2247FYF8452PB45247P387F8P6B2P7F82222472247FYF8F8B245F85454767P38F82452PB247BF822P722P3847FYF82457PBP38"};
 
 auto &input = inputB;
 
-const uint32_t numoftransitions = (sizeof(input)/sizeof(input[0])) - 1;
+const uint32_t numoftransitions = (sizeof(input) / sizeof(input[0])) - 1;
 const string inputstring = input[numoftransitions][0];
 
 void addTransformation(set<string> &resultset, const string inputstring, const string from, const string to, size_t maxsize = -1)
 {
     //cout << from << " ==> " << to << endl;
     auto pos = inputstring.find(from);
-    while ( string::npos != pos )
+    while (string::npos != pos)
     {
         string result = inputstring.substr(0, pos);
         result += to;
-        result += inputstring.substr(pos+from.size());
+        result += inputstring.substr(pos + from.size());
         //cout << result << endl;
 
-        if ( result.length() <= maxsize )
+        if (result.length() <= maxsize)
             resultset.insert(result);
 
-        pos = inputstring.find(from, pos+1);
+        pos = inputstring.find(from, pos + 1);
     }
 }
 
-int main(int, char**) {
+int main(int, char **)
+{
     int resultA = 0;
     int resultB = 0;
 
-    set<string> resultsetA; 
+    set<string> resultsetA;
     cout << inputstring << endl;
-    for ( auto elem : input )
+    for (auto elem : input)
     {
-        if ( elem[1] != "" )
+        if (elem[1] != "")
         {
             addTransformation(resultsetA, inputstring, elem[0], elem[1]);
         }
@@ -148,26 +145,26 @@ int main(int, char**) {
     cout << "Result A: " << resultA << endl;
 
     // Part 2
-    set<string> resultsetB[2]; 
-    uint8_t activeset=0;
+    set<string> resultsetB[2];
+    uint8_t activeset = 0;
     resultsetB[activeset].insert("e");
 
-    while ( resultsetB[activeset].end() == resultsetB[activeset].find(inputstring) )
+    while (resultsetB[activeset].end() == resultsetB[activeset].find(inputstring))
     {
         ++resultB;
 
-        resultsetB[1-activeset].clear();
-        
+        resultsetB[1 - activeset].clear();
+
         //for ( auto elem1 : resultsetB[activeset] )
-        for ( auto elem1 = resultsetB[activeset].begin(); elem1!=resultsetB[activeset].end(); )
+        for (auto elem1 = resultsetB[activeset].begin(); elem1 != resultsetB[activeset].end();)
         {
-            if ( *elem1!= "" )
+            if (*elem1 != "")
             {
-                for ( auto elem2 : input )
+                for (auto elem2 : input)
                 {
-                    if ( elem2[1] != "" )
-                    {        
-                        addTransformation(resultsetB[1-activeset], *elem1, elem2[0], elem2[1], inputstring.length() );
+                    if (elem2[1] != "")
+                    {
+                        addTransformation(resultsetB[1 - activeset], *elem1, elem2[0], elem2[1], inputstring.length());
                     }
                 }
 
