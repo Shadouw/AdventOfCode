f1abb1ab97e5f05169e31823e11466a6fad5c59b
Rolf Schatten <Rolf.Schatten@gmail.com>
2022-11-30 11:04:44 +0100

    New include folder and aocmd5.h

---
+++ include/aocmd5.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// Link against crypto to make MD5 function available
+
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <openssl/md5.h>
+
+std::string getmd5hexhash(const std::string input)
+{
+    static unsigned char md5_result[MD5_DIGEST_LENGTH];
+    MD5((const unsigned char *)input.c_str(), input.length(), md5_result);
+
+    std::stringstream retval;
+
+    for (auto c : md5_result)
+    {
+        retval << std::setfill('0') << std::setw(2) << std::hex << (unsigned int)c;
+    }
+
+    return retval.str();
+}
