3ab05b49149ff67f5022d575e58deeec25bfb61c
Rolf Schatten <Rolf.Schatten@gmail.com>
2023-12-01 09:13:43 +0100

    2023.01 Solved

---
+++ include/stringreplace.h
@@ -0,0 +1,23 @@
+// Stolen from: https://stackoverflow.com/questions/5878775/how-to-find-and-replace-string
+
+#pragma once
+#include <string>
+
+std::string ReplaceString(std::string subject, const std::string& search,
+                          const std::string& replace) {
+    size_t pos = 0;
+    while ((pos = subject.find(search, pos)) != std::string::npos) {
+         subject.replace(pos, search.length(), replace);
+         pos += replace.length();
+    }
+    return subject;
+}
+
+void ReplaceStringInPlace(std::string& subject, const std::string& search,
+                          const std::string& replace) {
+    size_t pos = 0;
+    while ((pos = subject.find(search, pos)) != std::string::npos) {
+         subject.replace(pos, search.length(), replace);
+         pos += replace.length();
+    }
+}
