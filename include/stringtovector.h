4e3942fdfcbcd4136d6afced22d327504d286a0b
Rolf Schatten <Rolf.Schatten@gmail.com>
2023-12-02 12:18:44 +0100

    2023.02: Part 1 Solved

---
+++ include/stringtovector.h
@@ -0,0 +1,21 @@
+// Stolen from
+// https://stackoverflow.com/questions/5607589/right-way-to-split-an-stdstring-into-a-vectorstring
+#pragma once
+
+#include <string>
+#include <sstream>
+#include <vector>
+
+std::vector <std::string> stringtovector(const std::string &inputstring, const char separator)
+{
+    std::string token;
+    std::stringstream sstream(inputstring);
+    std::vector <std::string>tokens;
+
+    while ( getline( sstream, token, separator ) )
+    {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
