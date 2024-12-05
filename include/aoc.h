88f3c5026c6521290333fb3ab1f232aae4a391a7
Shadouw <Rolf.Schatten@gmail.com>
2024-12-05 10:05:49 +0100

    2024.05 Part 1 solved

---
+++ include/aoc.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <cassert>
+#include <algorithm>
+
+#include <catch2/catch_test_macros.hpp>
+
+// #include <aocmd5.h> This is deprecated
+#include <hextoint.h>
+#include <kgv_ggt.h>
+#include <manhattan.h>
+#include <matrix.h>
+#include <stringreplace.h>
+#include <stringtovector.h>
