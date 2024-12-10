ac420c350a3bb011b646a7fee5370494bd9b0c82
Shadouw <Rolf.Schatten@gmail.com>
2024-12-10 14:56:09 +0100

    Fixes in matrix.h

--- include/matrix.h
+++ include/matrix.h
@@ -1,5 +1,7 @@
 #pragma once
 
+typedef std::pair<long, long> position;
+
 template <class T>
 class matrix {
 public:
@@ -10,18 +12,28 @@ public:
         m = new T[X * Y];
     }
 
-    //
-    // Add Assignement Operator!!!
-    //
-
-    matrix(const matrix& obj)
+    matrix(const matrix& other)
     {
-        X = obj.X;
-        Y = obj.Y;
+        X = other.X;
+        Y = other.Y;
         m = new T[X * Y];
 
-        for (size_t p = 0; p < X*Y; ++p)
-            m[p] = obj.m[p];
+        for (size_t p = 0; p < X * Y; ++p)
+            m[p] = other.m[p];
+    }
+
+    matrix& operator=(const matrix& other)
+    {
+        if (this != &other) {
+            X = other.X;
+            Y = other.Y;
+
+            delete[] m;
+            m = new T[X * Y];
+
+            std::copy(other.m, other.m + X * Y, m);
+        }
+        return *this;
     }
 
     std::size_t index(std::size_t x, std::size_t y) const
@@ -36,6 +48,20 @@ public:
     {
         for (std::size_t i = 0; i < X * Y; ++i)
             m[i] = value;
+    }
+
+    bool isValidPosition(position p) const
+    {
+        return (p.first >= 0 && p.first < X && p.second >= 0 && p.second < Y);
+    }
+
+    bool setIfValidCell(position p, T value)
+    {
+        if ( !isValidPosition(p) )
+            return false;
+
+        m[index(p.first, p.second)] = value;
+        return true;
     }
 
     void print() const
