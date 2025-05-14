6d9c6363cf7eab9027b35921125afffb211bac3e
Shadouw <Rolf.Schatten@gmail.com>
2025-05-14 11:22:52 +0200

    2024.01&Template: Data is read from files.

---
+++ include/aocdatafile.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <fstream>
+#include <vector>
+
+namespace aoc {
+
+class aocdatafile {
+public:
+    aocdatafile(std::string _basefilename, std::string _extension)
+        : basefilename(_basefilename)
+        , extension(_extension)
+    {
+        filename = basefilename.substr(0, basefilename.rfind(".")+1) + _extension;
+
+        std::ifstream infile(filename);
+
+        std::string line;
+        while (std::getline(infile, line)) {
+            lines.push_back(line);
+        }
+
+        std::cout << "Input file: " << filename << ", lines: " << lines.size()
+                  << std::endl;
+    }
+
+    std::vector<std::string> getLines()
+    {
+        return lines;
+    }
+
+    std::vector<std::vector<std::string>> getVectors()
+    {
+        std::vector<std::vector<std::string>> ret;
+
+        for (auto elem : lines)
+            ret.push_back(stringtovector(elem, ' '));
+
+        return ret;
+    }
+
+private:
+    std::string basefilename;
+    std::string extension;
+    std::string filename;
+    std::vector<std::string> lines;
+};
+
+} // namespace aoc
