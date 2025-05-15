3189493d53146f2621c94de888cf1c149866e357
Shadouw <Rolf.Schatten@gmail.com>
2025-05-15 13:06:08 +0200

    2019.01: Read data from files.

--- 2019/01.cpp
+++ 2019/01.cpp
@@ -1,144 +1,24 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
-#include <catch2/catch_test_macros.hpp>
-
+#include <aoc.h>
 using namespace std;
+using namespace aoc;
 
-const vector<long> inputA = {
-    83285,
-    96868,
-    121640,
-    51455,
-    128067,
-    128390,
-    141809,
-    52325,
-    68310,
-    140707,
-    124520,
-    149678,
-    87961,
-    52040,
-    133133,
-    52203,
-    117483,
-    85643,
-    84414,
-    86558,
-    65402,
-    122692,
-    88565,
-    61895,
-    126271,
-    128802,
-    140363,
-    109764,
-    53600,
-    114391,
-    98973,
-    124467,
-    99574,
-    69140,
-    144856,
-    56809,
-    149944,
-    138738,
-    128823,
-    82776,
-    77557,
-    51994,
-    74322,
-    64716,
-    114506,
-    124074,
-    73096,
-    97066,
-    96731,
-    149307,
-    135626,
-    121413,
-    69575,
-    98581,
-    50570,
-    60754,
-    94843,
-    72165,
-    146504,
-    53290,
-    63491,
-    50936,
-    79644,
-    119081,
-    70218,
-    85849,
-    133228,
-    114550,
-    131943,
-    67288,
-    68499,
-    80512,
-    148872,
-    99264,
-    119723,
-    68295,
-    90348,
-    146534,
-    52661,
-    99146,
-    95993,
-    130363,
-    78956,
-    126736,
-    82065,
-    77227,
-    129950,
-    97946,
-    132345,
-    107137,
-    79623,
-    148477,
-    88928,
-    118911,
-    75277,
-    97162,
-    80664,
-    149742,
-    88983,
-    74518};
-
-class problem
-{
+class TheTyrannyOfTheRocketEquation {
 public:
-    problem(const vector<long> &_input) : input(_input)
+    TheTyrannyOfTheRocketEquation(string _file, string _extension)
+        : datafile(_file, _extension)
     {
-        cout << "Size of Input: " << input.size() << endl;
+        for (auto elem : datafile.getLines())
+            input.push_back(atol(elem.c_str()));
     }
 
-    void listinput()
+    const vector<long> &getInput()
     {
-        for (auto elem : input)
-        {
-            cout << elem << endl;
-        }
-    }
-
-    int getresultA()
-    {
-        cout << "resultA: " << resultA << endl;
-        return resultA;
-    }
-    int getresultB()
-    {
-        cout << "resultB: " << resultB << endl;
-        return resultB;
+        return input;
     }
 
 private:
-    const vector<long> input;
-    int resultA = 0;
-    int resultB = 0;
+    aocdatafile datafile;
+    vector<long> input;
 };
 
 long calcFuel(const long mass)
@@ -150,8 +30,7 @@ long calcRecursiveFuel(long mass)
 {
     long ret = 0;
 
-    while (mass > 0)
-    {
+    while (mass > 0) {
         mass = calcFuel(mass);
         if (mass > 0)
             ret += mass;
@@ -180,9 +59,11 @@ TEST_CASE("Testdata")
     REQUIRE(33583 == calcFuel(100756));
 }
 
-TEST_CASE("Problem A")
+TEST_CASE("TheTyrannyOfTheRocketEquation A")
 {
-    REQUIRE(3289802 == calcFuel(inputA));
+    TheTyrannyOfTheRocketEquation thetyrannyoftherocketequation(__FILE__, "mydata");
+
+    REQUIRE(3289802 == calcFuel(thetyrannyoftherocketequation.getInput()));
 }
 
 TEST_CASE("Testdata Fuel for Fuel")
@@ -192,7 +73,8 @@ TEST_CASE("Testdata Fuel for Fuel")
     REQUIRE(50346 == calcRecursiveFuel(100756));
 }
 
-TEST_CASE("Problem B")
+TEST_CASE("TheTyrannyOfTheRocketEquation B")
 {
-    REQUIRE(4931831 == calcFuel(inputA, true));
+    TheTyrannyOfTheRocketEquation thetyrannyoftherocketequation(__FILE__, "mydata");
+    REQUIRE(4931831 == calcFuel(thetyrannyoftherocketequation.getInput(), true));
 }
